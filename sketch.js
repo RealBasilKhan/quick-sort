56cad1d1581f920f26c2eb8fe492548c5a5e0184
Basil Khan <basilkhan76@gmail.com>
2021-08-09 16:14:32 -0400

    improving visualization

--- sketch.js
+++ sketch.js
@@ -11,25 +11,28 @@ function setup() {
   quickSort(values, 0, values.length - 1);
 }
 
-function quickSort(arr, start, end) {
+async function quickSort(arr, start, end) {
   if (start >= end) {
     return;
   }
-  let index = partition(arr, start, end);
-  quickSort(arr, start, index - 1);
-  quickSort(arr, index + 1, end);
+  let index = await partition(arr, start, end);
+
+  await Promise.all([
+    await quickSort(arr, start, index - 1),
+    await quickSort(arr, index + 1, end),
+  ]);
 }
 
-function partition(arr, start, end) {
+async function partition(arr, start, end) {
   let pivotValue = arr[end];
   let pivotIndex = start;
   for (let i = start; i < end; i++) {
     if (arr[i] < pivotValue) {
-      swap(arr, i, pivotIndex);
+      await swap(arr, i, pivotIndex);
       pivotIndex++;
     }
   }
-  swap(arr, pivotIndex, end);
+  await swap(arr, pivotIndex, end);
   return pivotIndex;
 }
 
@@ -43,8 +46,13 @@ function draw() {
   }
 }
 
-function swap(arr, a, b) {
+async function swap(arr, a, b) {
+  await sleep(25);
   let temp = arr[a];
   arr[a] = arr[b];
   arr[b] = temp;
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
