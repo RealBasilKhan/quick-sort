a34f111e7e303e1dd8a61511b762a7bce78cd653
Basil Khan <basilkhan76@gmail.com>
2021-08-09 16:35:25 -0400

    Sorting Algo Finished

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
-let i = 0;
+let values = [];
 let w = 10;
 
+let states = [];
+
 function setup() {
-  createCanvas(800, 200);
+  createCanvas(windowWidth, windowHeight);
   values = new Array(floor(width / w));
   for (let i = 0; i < values.length; i++) {
     values[i] = random(height);
+    states[i] = -1;
   }
-  //frameRate(5);
   quickSort(values, 0, values.length - 1);
 }
 
@@ -16,38 +18,59 @@ async function quickSort(arr, start, end) {
     return;
   }
   let index = await partition(arr, start, end);
+  states[index] = -1;
 
   await Promise.all([
-    await quickSort(arr, start, index - 1),
-    await quickSort(arr, index + 1, end),
+    quickSort(arr, start, index - 1),
+    quickSort(arr, index + 1, end),
   ]);
 }
 
 async function partition(arr, start, end) {
+  for (let i = start; i < end; i++) {
+    states[i] = 1;
+  }
+
   let pivotValue = arr[end];
   let pivotIndex = start;
+  states[pivotIndex] = 0;
   for (let i = start; i < end; i++) {
     if (arr[i] < pivotValue) {
       await swap(arr, i, pivotIndex);
+      states[pivotIndex] = -1;
       pivotIndex++;
+      states[pivotIndex] = 0;
     }
   }
   await swap(arr, pivotIndex, end);
+
+  for (let i = start; i < end; i++) {
+    if (i != pivotIndex) {
+      states[i] = -1;
+    }
+  }
+
   return pivotIndex;
 }
 
 function draw() {
-  background(51);
+  background(0);
 
   for (let i = 0; i < values.length; i++) {
-    stroke(0);
-    fill(255);
+    noStroke();
+    if (states[i] == 0) {
+      fill("#E0777D");
+    } else if (states[i] == 1) {
+      fill("#D6FFB7");
+    } else {
+      fill(255);
+    }
     rect(i * w, height - values[i], w, values[i]);
   }
 }
 
 async function swap(arr, a, b) {
-  await sleep(25);
+  await sleep(50);
   let temp = arr[a];
   arr[a] = arr[b];
   arr[b] = temp;
